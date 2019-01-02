74369d70aedb9866f33573f3ba85069c7906a9a8
Tera Coder <31596711+TerableCoder@users.noreply.github.com>
2019-01-02 10:29:32 -0500

    fix

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ module.exports = function AfkBg(dispatch) {
     let location = null,
         locRealTime = 0
     
-    dispatch.hook('C_PLAYER_LOCATION', 2, event => {
+    dispatch.hook('C_PLAYER_LOCATION', 5, event => {
         location = event
         locRealTime = Date.now()
     })
     
     dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
         if (event.message.startsWith("@1636")) {
-            dispatch.toServer('C_PLAYER_LOCATION', 2, Object.assign({}, location, {
+            dispatch.toServer('C_PLAYER_LOCATION', 5, Object.assign({}, location, {
                 type: 2,
                 time: location.time - locRealTime + Date.now() - 50
             }))
-            dispatch.toServer('C_PLAYER_LOCATION', 2, Object.assign(location, {
+            dispatch.toServer('C_PLAYER_LOCATION', 5, Object.assign(location, {
                 type: 7,
                 time: location.time - locRealTime + Date.now() + 50
             }))
